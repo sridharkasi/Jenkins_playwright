39ea85828649a1ca82308d93240ab8ca92fc5c44
Kasinathan <skasinathan@dxc.com>
2025-10-07 11:01:26 +0530

    second commit

---
+++ jenkinsfile_nodocker.groovy
@@ -0,0 +1,66 @@
+pipeline {
+  agent any
+  stages {
+    stage('Checkout') {
+      steps {
+        checkout scm
+      }
+    }
+
+    stage('Start demo app') {
+      steps {
+        // start a simple static server in background and record pid
+        sh '''
+          nohup python3 -m http.server 5000 --directory site --bind 0.0.0.0 > app.log 2>&1 &
+          echo $! > app.pid
+          # wait for server to boot (simple check)
+          for i in 1 2 3 4 5; do
+            curl -sS http://127.0.0.1:5000 && break || sleep 1
+          done
+        '''
+      }
+    }
+	
+	stages {
+        stage('Setup Python') {
+            steps {
+                sh '''
+                  python3 -m venv venv
+                  source venv/bin/activate
+                  pip install --upgrade pip
+                  pip install -r requirements.txt
+				  # Install Playwright browsers (Chromium with dependencies)
+                  playwright install chromium --with-deps
+                '''
+            }
+        }
+		
+		
+	
+    stage('Run Playwright tests) {
+      steps {
+        // Use a Playwright python image to run tests. Replace version if needed.
+         sh '''
+            #source venv/bin/activate
+            test_playwright.py
+                '''
+      }
+    }
+
+    stage('Teardown') {
+      steps {
+        sh '''
+          if [ -f app.pid ]; then
+            kill $(cat app.pid) || true
+            rm -f app.pid
+          fi
+        '''
+      }
+    }
+  } // stages
+  post {
+    always {
+      archiveArtifacts artifacts: 'app.log', allowEmptyArchive: true
+    }
+  }
+}
